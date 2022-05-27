35b54e305e2bc0fea1ea78ba3fb38b17ddd68417
r0otx <r00tx@mail.ru>
2022-05-27 13:44:51 +0400

    Push script

---
+++ index.js
@@ -0,0 +1,18 @@
+/*
+Шагаем на URL: https://vk.com/feed?section=likes
+Открываем в браузере консоль Ctrl+I
+В нее вводим код ниже. Если вылезает капча, в 13 строке меняет setInterval
+на значение больше (1000ms = 1сек)
+Реализован автоскрол страницы и удаление лайков.
+*/
+
+function removeLikes() {
+    let deletePostLink = document.body.querySelectorAll('div.PostBottomAction._like.active[onclick^="Likes.toggle"]');
+    for (let i = 0; i < deletePostLink.length; i++) {
+        deletePostLink[i].click();
+    }
+    console.log(deletePostLink.length + ' likes deleted');
+    window.scrollTo(0, document.body.scrollHeight);
+    setTimeout(removeLikes, 10000);
+}
+removeLikes();
